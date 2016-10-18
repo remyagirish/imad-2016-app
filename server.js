27a547e309ff5def8f84f2e9e2b8d3ba8fee2de1
remyagirish <remyagirish@gmail.com>
2016-10-18 17:52:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,9 @@ app.get('/:articleName', function(req,res){
 app.get('/articletwo', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
+app.get('/ui/main.js', function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/articlethree', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
 });
