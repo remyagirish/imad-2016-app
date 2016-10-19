5238b83deda1531d31eed689ed52e4d1de4542d1
remyagirish <remyagirish@gmail.com>
2016-10-19 11:13:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,8 +88,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/black.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'black.jpg'));
 });
 
 
