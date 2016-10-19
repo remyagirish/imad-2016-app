981cd663e047e6fc7b91e5c92bf25e8bd01324f1
remyagirish <remyagirish@gmail.com>
2016-10-19 12:21:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
-var im = document.getElementById('image');
-im.onclick = function()
-{
-    im.style.marginLeft="100px";
+
+//Move the image
+var img = document.getElementById('image');
+img.onclick = function(){
+    img.style.marginLeft='100px';
 };
