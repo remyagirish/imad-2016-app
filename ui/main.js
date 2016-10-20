dbf67d48b81c3551c2b516643222a9dc997b5747
remyagirish <remyagirish@gmail.com>
2016-10-20 18:55:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,20 @@
 
  console.log('Loaded!');
  window.onload=function(){
-    
-     //Move the image
-    var img = document.getElementById("image");
-    img.onclick=function(){
-        img.style.mariginLeft='100px';
-      //  var interval=setInterval(movRight,50);
-      
+        
+         //Move the image
+        var img = document.getElementById("image");
+        img.onclick=function(){
+            img.style.mariginLeft='100px';
+          //  var interval=setInterval(movRight,50);
+        };  
+       
          var button = document.getElementById("counter");
          var counter =0;
-            button.onclick = function() {
+         button.onclick = function() {
                 counter = counter + 1;
-                            var span = document.getElementById("count");
-                            span.innerHTML = counter.toString();
-                    };
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+               };
    
-   };
- };
+    };
