ab35b7af58c421faa74ee5410a4e47a3707e4685
remyagirish <remyagirish@gmail.com>
2016-10-20 14:42:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,14 @@
 console.log('Loaded!');
 
-//Move the image
-var img = document.getElementById('image');
-img.onClick = function(){
-    img.style.marginLeft='100px';
+
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+  
+  //make a request to the counter endpoint;
+  //capture the response and store it in a variable
+  //render the variable in the correct span
+  counter=counter+1;
+  var span=document.getElementId('count');
+  span.innerHTML=counter.toString();
 };
