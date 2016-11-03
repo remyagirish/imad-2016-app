99c3b64a0a508cdfa4f7ff0f5d50694e7ca8b7db
remyagirish <remyagirish@gmail.com>
2016-11-03 21:15:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/test-db',function(req,res){
             res.status(500).send(err.toString()); 
             
         } else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });
