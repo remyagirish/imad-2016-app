db802036907e661b143779dce8e539850f1fd5da
remyagirish <remyagirish@gmail.com>
2016-11-03 20:49:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={
+    user:'remyagirish',
+    database:'remyagirish',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.eny.DB_PASSWORD
+    
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -67,6 +75,20 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool = new Pool(config);
+
+app.get9'/test-db',function(req,res){
+    //made a select request
+    //return a response with the results
+    pool.query('select * from test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+    });
+};
+
 var counter =0;
 app.get('/counter', function(req,res){
    counter=counter+1;
