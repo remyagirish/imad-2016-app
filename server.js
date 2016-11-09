6a63deb6436734d3bb68736d2dc5677170f2285d
remyagirish <remyagirish@gmail.com>
2016-11-09 13:23:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ var names
 app.get('/articles/:articleName', function(req,res){
     //articleName==articleone
     //articles[articleName]=={}content object for article one
-    pool.query("select * from article where title=" + req.pramas.articleName,function(err,result){
+    pool.query("select * from article where title='" + req.pramas.articleName +"'",function(err,result){
         if(err){
             res.status(500).send(err.toString());
             } else {
@@ -113,15 +113,15 @@ app.get('/articles/:articleName', function(req,res){
         });
     });
   
-app.get('/articletwo', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
-});
+//app.get('/articletwo', function(req,res){
+//   res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
+//});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/articlethree', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
-});
+//app.get('/articlethree', function(req,res){
+  // res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
+//});
 
 
 app.get('/ui/style.css', function (req, res) {
