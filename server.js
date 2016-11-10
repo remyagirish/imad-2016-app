694b477300a9d40c0a9475ec11e9a3ccc2ff1a46
remyagirish <remyagirish@gmail.com>
2016-11-10 14:04:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/articles/:articleName', function(req,res){
     //articleName==articleone
     //articles[articleName]=={}content object for article one
     //var articleName;
-    pool.query("select * from article where title=" + req.pramas.articleName,function(err,result){
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName,function(err,result){
         if(err){
             res.status(500).send(err.toString());
             } else {
