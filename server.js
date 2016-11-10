22836969d69ee17caf7cf41f8b01877ef9c5b5c4
remyagirish <remyagirish@gmail.com>
2016-11-10 22:39:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/articles/:articleName', function(req,res){
                 if(result.rows.length===0){
                     res.status(404).send('Article not Found');
                 } else {
-                    var articleData=result.rows[];
+                    var articleData=result.rows[0];
                     res.send(createTemplate(articleData));
                 }
             }
