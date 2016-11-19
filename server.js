dd3cc9d7622de93b556fe5025ddc69f162d62e03
remyagirish <remyagirish@gmail.com>
2016-11-19 10:53:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ var pool = new Pool(config);
 function hash(input, salt){
     //How do we create a  hash?
     var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return["pbkdf2","10000",salt,hashed.toString('hex')].join('$') ;
     
 }
 
